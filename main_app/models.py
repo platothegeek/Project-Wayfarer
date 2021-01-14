73aeefdfd0b5329d58673a995dcdeb4130ddb480
Doireann Herold <ivydoor@icloud.com>
2021-01-13 16:43:21 -0800

    Second Initial Commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class City:
+    def __init__(self, name, state):
+        self.name = name 
+        self.state = state
+
+cities = [
+    City('San Francisco', 'California')
+]
+
+# users = [
+#     User('Bob', 'Smith', "Lorem Epsom", "New York City")
+# ]
+
+# class User:
+#     def __init__(self, first_name, last_name, posts, current_city):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.posts = posts
+#         self.current_city = current_city
+
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import City
+
 # Create your views here.
 
 def home(request):
