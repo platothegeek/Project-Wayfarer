25a6a407364ad962b8aabb59324a5a419f648945
platothegeek <73858489+platothegeek@users.noreply.github.com>
2021-03-11 09:01:46 -0800

    Update settings.py

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9aw&mcofmo9jwgk5c@k4bp&qps8j5s&+s8^l27yzziu7^q$l0j'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
